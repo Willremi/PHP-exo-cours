1e46ae9cded994f1fac5afefa388cfa950565a5a
popschool <popschool@debian>
2017-08-17 11:06:27 +0200

    add array.php

--- 08-18-2017/array.php
+++ 08-18-2017/array.php
@@ -1,13 +1,27 @@
 <?php
-
+// Création d'un tableau vide
 $myArray = [];
 
+
+// Ajout d'éléments
 $myArray[] = 42;
 $myArray[] = 3.14;
 $myArray[] = "foor bar baz";
 $myArray[] = true;
 
+// Lecture du tableau ave un index numérique
 for($i = 0; $i < count($myArray); $i++){
   echo $myArray[$i];
+  echo '<br />';
+}
+
+// Suppression de la donnée située à l'index 2
+array_splice($myArray, 2, 1);
+
+// Lecture du tableau ave une boucle foreach
+foreach($myArray as $key => $value){
+  echo $key;
+  echo '<br />';
+  echo $value;
   echo '<br />';
 }
