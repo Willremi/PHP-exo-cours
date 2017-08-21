f29ab79269b1615672d79e17227c13b069d13f7b
popschool <popschool@debian>
2017-08-21 11:05:02 +0200

    add form-01.php

--- 08-21-2017/form-01.php
+++ 08-21-2017/form-01.php
@@ -1,3 +1,21 @@
+<?php
+
+$title = '';
+$body = '';
+
+if($_GET){
+// var_dump($_GET);
+
+if(isset($_GET['title'])) {
+$title = $_GET['title'];
+}
+if(isset($_GET['body'])) {
+$body = $_GET['body'];
+}
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -8,9 +26,9 @@
 
     <form action="" method="get">
 
-      <input type="text" name="title" value="" placeholder="titre"/><br />
-      <textarea name="body" placeholder="texte"></textarea><br />
-      <input type="sbmit" value="Envoyer" />
+      <input type="text" name="title" value="<?=htmlentities($title)?>" placeholder="titre"/><br />
+      <textarea name="body" placeholder="texte"><?=htmlentities($body)?></textarea><br />
+      <input type="submit" value="Envoyer" />
 
     </form>
 
